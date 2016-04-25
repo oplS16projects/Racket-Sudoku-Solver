6ad1b616880d3d78cf5a2b357ba2fe1c89368356
Yusuf Yildiz <yusuf_yildiz@student.uml.edu>
2016-04-25 13:18:11 -0400

    Implemented grid drawing in recursive method rather than manual implementation

--- sudoku_solver_gui.rkt
+++ sudoku_solver_gui.rkt
@@ -78,8 +78,18 @@
 (define (print-ind val t-table x y dc s)
   (if (equal? val 0) (set! val " ") #f)
   (if (not(is-given? t-table x y))
-      (send dc draw-text (if (equal? val " ") val (number->string val)) (+ (* (/ x 9) s) 20) (+ (* (/ y 9) s) 15))
+      (begin
+        (send dc set-brush (new brush% [color "white"]))
+        (let ((x (* (/ 1 9) s)) (y (* (/ 1 9) s)))
+          (send dc draw-rectangle x y y x)
+          (send dc set-pen "white" 1 'transparent))
+        (send dc draw-text (if (equal? val " ") val (number->string val)) (+ (* (/ x 9) s) 20) (+ (* (/ y 9) s) 15)))
       (begin 
+        ;;; Make sure that we erase previous data before drawing
+        (send dc set-brush (new brush% [color "white"]))
+        (let ((x (* (/ 1 9) s)) (y (* (/ 1 9) s)))
+          (send dc draw-rectangle x y y x)
+          (send dc set-pen "white" 1 'transparent))
         (send dc set-text-foreground "green")
         (send dc draw-text (if (equal? val " ") val (number->string val)) (+ (* (/ x 9) s) 20) (+ (* (/ y 9) s) 15))
         (send dc set-text-foreground "black"))))
